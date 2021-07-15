4586637e416515e9d72b12332ebaf3e57286d8ce
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-07-16 00:33:03 +0530

    Update zombie.py

--- mrjoker/modules/zombie.py
+++ mrjoker/modules/zombie.py
@@ -5,7 +5,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChannelParticipantsAdmins, ChatBannedRights
 
-from Villain import DEMONS, DEV_USERS, DRAGONS, OWNER_ID, telethn
+from mrjoker import DEMONS, DEV_USERS, DRAGONS, OWNER_ID, telethn
 
 # =================== CONSTANT ===================
 
