f19171680c364b813688eccacec1d7b3c8bcea71
kavinduaj12 <86953304+kavinduaj12@users.noreply.github.com>
2021-07-15 23:30:49 +0530

    Update handlers.py

--- mrjoker/modules/helper_funcs/handlers.py
+++ mrjoker/modules/helper_funcs/handlers.py
@@ -8,8 +8,8 @@ from pyrate_limiter import (
 from telegram import Update
 from telegram.ext import CommandHandler, Filters, MessageHandler, RegexHandler
 
-import Villain.modules.sql.blacklistusers_sql as sql
-from Villain import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
+import mrjoker.modules.sql.blacklistusers_sql as sql
+from mrjoker import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 
 if ALLOW_EXCL:
     CMD_STARTERS = ("/", "!")
