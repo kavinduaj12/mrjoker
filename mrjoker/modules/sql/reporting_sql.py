72156d526bfea822820f82503652ec79b0f91909
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-07-16 00:21:23 +0530

    Update reporting_sql.py

--- mrjoker/modules/sql/reporting_sql.py
+++ mrjoker/modules/sql/reporting_sql.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from sqlalchemy import Boolean, Column, Integer, String
 
-from Villain.modules.sql import BASE, SESSION
+from mrjoker.modules.sql import BASE, SESSION
 
 
 class ReportingUserSettings(BASE):
