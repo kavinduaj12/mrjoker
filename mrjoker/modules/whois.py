edb9457b824e3601fe95af0aad335c8364ca7278
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-07-16 00:36:57 +0530

    Update whois.py

--- mrjoker/modules/whois.py
+++ mrjoker/modules/whois.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.errors import PeerIdInvalid
 from pyrogram.types import Message, User
 
-from Villain import pbot
+from mrjoker import pbot
 
 
 def ReplyCheck(message: Message):
