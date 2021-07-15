4291d9cfe44057385ae57af94a0a840809380a39
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-07-16 01:23:06 +0530

    Update dev.py

--- mrjoker/modules/dev.py
+++ mrjoker/modules/dev.py
@@ -6,8 +6,8 @@ from time import sleep
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from Villain import dispatcher
-from Villain.modules.helper_funcs.chat_status import dev_plus
+from mrjoker import dispatcher
+from mrjoker.modules.helper_funcs.chat_status import dev_plus
 
 
 @run_async
