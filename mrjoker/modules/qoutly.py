8b6e3648f9ca52ceddfd0aab5bea667b324fbdf5
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-07-16 01:01:44 +0530

    Update qoutly.py

--- mrjoker/modules/qoutly.py
+++ mrjoker/modules/qoutly.py
@@ -9,7 +9,7 @@ from fontTools.ttLib import TTFont
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import functions, types
 
-from Villain.events import register
+from mrjoker.events import register
 
 COLORS = [
     "#F07975",
